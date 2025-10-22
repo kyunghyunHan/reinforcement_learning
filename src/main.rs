8dec40e4987990bb309d00ab7a7674fa210de984
kyunghyunHan <ekfqlwcjswl@naver.com>
2025-10-22 22:37:48 +1100

    feat:update

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use rl_rs::{model_free::{banpit, dynamic_programming,grid_word}, q_learning};
 fn main() {
-    // q_learning::example();
+    q_learning::example();
     banpit::example();
     // dynamic_programming::example();
     // grid_word::example();
