31368b8df88d0b00907fc9a90638fbc798e4d29b
kyunghyunHan <ekfqlwcjswl@naver.com>
2025-11-03 17:33:25 +1100

    feat:update

--- src/model_free/bandit.rs
+++ src/model_free/bandit.rs
@@ -12,71 +12,28 @@ use ndarray::{Array1, Array2};
 use rand::rngs::StdRng;
 use rand::Rng;
 use rand::SeedableRng;
-pub struct Bandit {
-    rates: Array1<f32>,
+
+use crate::model_free::bandit;
+
+struct Bandit {
+    rates: Array1<f64>,
 }
 
-const ACTION_SIZE: usize = 10;
-
 impl Bandit {
-    pub fn new() -> Self {
-        let mut rng = rand::thread_rng();
-        let rates: Array1<f32> = Array1::from_shape_fn(ACTION_SIZE, |_| rng.gen_range(0.0..=1.));
+    fn new(arms: usize) -> Self {
+        let mut rng = StdRng::seed_from_u64(0);
+        let rates = Array1::from_shape_fn(arms, |_| rng.gen_range(0.0..1.0));
         Self { rates }
     }
 
-    pub fn play(&self, arm: usize) -> usize {
-        let rate = self.rates[arm];
+    fn play(&self, arm: usize) -> usize {
         let mut rng = rand::thread_rng();
-        let random_value = rng.gen_range(0.0..=1.0);
+        let rate = self.rates[arm];
 
-        if rate as f64 > random_value {
+        if rate > rng.gen_range(0.0..1.0) {
             1
         } else {
             0
-        }
-    }
-}
-
-struct Agent {
-    epsilon: f32,
-    qs: Array1<f32>,
-    ns: Array1<usize>,
-}
-
-impl Agent {
-    fn new(epsilon: f32) -> Self {
-        Self {
-            epsilon,
-            qs: Array1::zeros(ACTION_SIZE),
-            ns: Array1::zeros(ACTION_SIZE),
-        }
-    }
-
-    fn update(&mut self, action: usize, reward: usize) {
-        self.ns[action] += 1;
-        let alpha = 1.0 / self.ns[action] as f32;
-        self.qs[action] += (reward as f32 - self.qs[action]) * alpha;
-    }
-
-    fn get_action(&mut self) -> usize {
-        let mut rng = rand::thread_rng();
-        let random_value = rng.gen_range(0.0..=1.0);
-
-        if random_value < self.epsilon {
-            rng.gen_range(0..self.qs.len())
-        } else {
-            let mut best_index = 0;
-            let mut best_value = f32::NEG_INFINITY;
-
-            for (idx, &value) in self.qs.iter().enumerate() {
-                if value > best_value {
-                    best_index = idx;
-                    best_value = value;
-                }
-            }
-
-            best_index
         }
     }
 }
@@ -108,7 +65,8 @@ pub fn example() {
 
     // println!("총 보상 평균: {:?}", all_rates.mean());
     // test();
-    test2();
+
+    test3();
 }
 
 fn test() {
@@ -141,5 +99,13 @@ fn test2() {
         q = q + (reward - q) / n as f64;
 
         println!("step {n}: Q = {q}");
+    }
+}
+
+fn test3() {
+    let bandit = Bandit::new(10);
+    for i in 0..3 {
+        let reward = bandit.play(0);
+        println!("{}", reward);
     }
 }
